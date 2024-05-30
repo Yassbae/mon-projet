7b4d7b5e579c49dadc4ebf2e18a6733e967686f5
Takarli <takarliy9@gmail.com>
2024-05-30 09:03:24 +0200

    Marie: Modifications de la fonction recherche_binaire

--- recherche_binaire.py
+++ recherche_binaire.py
@@ -1,1 +1,17 @@
-def recherche_binaire(arr, x):
+def recherche_binaire(arr, x, debug=False):
+    # Modifications par Marie
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == x:
+            if debug:
+                print(f'Trouvé à l'index {mid}')
+            return mid
+        elif arr[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+    if debug:
+        print('Non trouvé')
+    return -1
