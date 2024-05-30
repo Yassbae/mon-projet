f29ee195fcbbc9dd1e74a606bbeb2c30a65f12ee
Takarli <takarliy9@gmail.com>
2024-05-30 08:54:36 +0200

    Jean - ajout de recherche_binaire

--- recherche_binaire.py
+++ recherche_binaire.py
@@ -1,12 +1,1 @@
-def recherche_binaire(arr, valeur):
-    debut = 0
-    fin = len(arr) - 1
-    while debut <= fin:
-        milieu = debut + (fin - debut) // 2
-        if arr[milieu] == valeur:
-            return milieu
-        elif arr[milieu] < valeur:
-            debut = milieu + 1
-        else:
-            fin = milieu - 1
-    return -1
+def recherche_binaire(arr, x):
