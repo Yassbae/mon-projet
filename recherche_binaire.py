5b0211e31797a96203cc9b2e4d7fb374e09397c3
Takarli <takarliy9@gmail.com>
2024-05-30 08:38:49 +0200

    Ajout de la fonction recherche_binaire

--- recherche_binaire.py
+++ recherche_binaire.py
@@ -1,1 +1,12 @@
-print('Hello, world!')
+def recherche_binaire(arr, x):
+    l = 0
+    r = len(arr) - 1
+    while l <= r:
+        mid = l + (r - l) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
